f09a0bd60c260c93c3c7d0d322727a45cc226ae9
Collin Cramer <forcollin28@outlook.com>
2024-02-14 14:56:27 -0700

    progress 2/14

---
+++ Module3/meme-generator-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
