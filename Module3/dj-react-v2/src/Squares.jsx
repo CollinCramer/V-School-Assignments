eab2f855d9aa2814b0f1effdd191dd0699faa661
Collin Cramer <forcollin28@outlook.com>
2024-03-21 17:06:47 -0600

    Progress 3-21

--- Module3/dj-react-v2/src/Squares.jsx
+++ Module3/dj-react-v2/src/Squares.jsx
@@ -8,16 +8,62 @@ export default function Squares () {
     const squareElements = squares.map(square => (
         <div style={{backgroundColor: `${square.backgroundColor}`}} className="box" key={square.id}></div>
     ))
-    // function smallTime() {
-    //     if 
-    // }
-   
+    function smallTime() {
+    //    if first square black, change all white
+    // 1. check background color of first square
+        const firstSquare = squares[0]
+        const squareBackground = firstSquare.backgroundColor
+    // if square is black, change all to white
+        if (squareBackground === "black") {
+            // create a new array of squares with background color as white
+            const squaresWhite = squares.map(square => {
+                return {
+                    ...square, 
+                    backgroundColor:"white"
+                }
+            })
+            // call set squares with new array
+            setSquares(squaresWhite)
+            return 
+        }
+        // if first square is not black, change all to black
+        const squaresBlack = squares.map(square => {
+        return {
+                ...square, 
+                backgroundColor:"black"
+            }
+        })
+        // call set squares with new array
+        setSquares(squaresBlack)
+    }
+   function partyDj() {
+    // change the first two squares purple
+    // map to create a new array
+    const squaresPurple = squares.map((square, index)=>{
+        // determine what index we are
+        // make a variable called backgroundColor
+        let backgroundColor = square.backgroundColor
+        // if index is 0 or 1, set backgroundColor to purple
+        if(index === 0 || index === 1) {
+            backgroundColor = "purple"
+        }
+        // else set background color to the squares background color
+        return {
+            ...square,
+            backgroundColor:backgroundColor
+        }
+    })
+    // spread in square properties
+    // change background color of index 0 and 1 to purple
+    // set squares to our new array
+    setSquares(squaresPurple)
+   }
     return (
         <div>
             {squareElements}
             <div className="buttons--container">
-                <button>Small Time DJ</button>
-                <button>Party DJ</button>
+                <button onClick={smallTime}>Small Time DJ</button>
+                <button onClick={partyDj}>Party DJ</button>
                 <button>Professional DJ</button>
                 <button>Big Time 1</button>
                 <button>Big Time 2</button>
---
+++ edabit-code-challenges/function-practice.js
@@ -0,0 +1,117 @@
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function getProperties() {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+// anonymous function
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+const array = [1,2,3]
+
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
