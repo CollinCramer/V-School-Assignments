34e4ddd97eb8976568a31df1b8941c4d9922c70a
Collin Cramer <forcollin28@outlook.com>
2024-03-28 16:49:59 -0600

    Progress 3-28

--- Module3/name-entry/src/App.jsx
+++ Module3/name-entry/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = React.useState({
+    inputContents:""
+  })
+  const [submissions, setSubmissions]= React.useState([])
+
+  function handleChange(event) {
+    const {name,value} = event.target
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value
+    }))
+  }
+  function handleClick() {
+    // check what is currently typed in the input box
+    const currentValue=formData.inputContents
+    // submit that to be displayed on the ordered list
+    const newSubmissions=[...submissions]
+    newSubmissions.push(currentValue)
+    setSubmissions(newSubmissions)
+    // clear input box
+    const clearFormData={...formData}
+    clearFormData.inputContents=""
+    setFormData(clearFormData)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <form>
+        <input
+          type="inputContents"
+          placeholder='Add Item'
+          className="form--input"
+          name="inputContents"
+          onChange={handleChange}
+          value={formData.inputContents}
+        />
+        <h1>{formData.inputContents}</h1>
+        <button 
+        type='button'
+        onClick={handleClick}
+        >Submit
+        
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <ol>
+         {submissions.map((element) => {
+          return (
+            <li>{element}</li>
+          )
+         })}
+        </ol>
+      </form>
+    </div>
   )
 }
 
--- edabit-code-challenges/function-practice.js
+++ edabit-code-challenges/function-practice.js
@@ -393,3 +393,371 @@ function map(element, index, array) {
 () => {
 
 }
+
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+(element, index, array) => {
+
+}
+
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+function map(element, index, array) {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+() => {
+
+}
+
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+function name() {
+
+}
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) =>{
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
+array.map((element, index, array) => {
+
+})
